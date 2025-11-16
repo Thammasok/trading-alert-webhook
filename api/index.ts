2680dd585d7e1226adf9a98bf7d629be52794f45
Charanchai Nutz <jaranchai.nt@hotmail.com>
2025-11-16 21:37:45 +0700

    chore: delete comment and scripts

--- api/index.ts
+++ api/index.ts
@@ -7,14 +7,3 @@ export default new Elysia()
   .get('/', () => 'Hello Elysia!')
   .get('/api/v1/webhook', (context) => LineCtr.webhook(context))
   .compile()
-
-// const app = new Elysia()
-
-// app.get('/', () => {
-//   return 'hello'
-// })
-// app.get('/api/v1/webhook', (context) => LineCtr.webhook(context))
-
-// app.listen(3000)
-
-// console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
