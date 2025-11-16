b096e14b0ae439aed0b48bf92996c228f3f20329
Charanchai Nutz <jaranchai.nt@hotmail.com>
2025-11-16 21:53:13 +0700

    feat: upgrade libs and edit return

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import { swagger } from '@elysiajs/swagger'
 
 export default new Elysia()
   .use(swagger())
-  .get('/', () => 'Hello Elysia!')
+  .get('/', () => 'Hello')
   .post('/mirror', ({ body }) => body, {
     body: t.Object({
       name: t.String()
