24e269a4cc4f846bd4c504ec50419189b8bec9d3
Charanchai Nutz <jaranchai.nt@hotmail.com>
2025-11-16 22:01:56 +0700

    feat: change route to webhook

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,8 @@
-import { Elysia, t } from 'elysia'
+import { Elysia } from 'elysia'
 import { swagger } from '@elysiajs/swagger'
 
 export default new Elysia()
   .use(swagger())
   .get('/', () => 'Hello')
-  .post('/mirror', ({ body }) => body, {
-    body: t.Object({
-      name: t.String()
-    })
-  })
+  .post('/api/v1/webhook', ({ body }) => console.log(body))
   .compile()
