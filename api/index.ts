5a281e8cd87f85658030d155a4844ea2b0003a23
Charanchai Nutz <jaranchai.nt@hotmail.com>
2026-01-31 11:22:04 +0700

    feat: add line bot sdk

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,29 @@
 import { Elysia } from 'elysia'
 import { swagger } from '@elysiajs/swagger'
+import * as Line from '@line/bot-sdk'
+
+// create LINE SDK config from env variables
+const config: Line.MiddlewareConfig = {
+  channelSecret: process.env.CHANNEL_SECRET as string
+}
+
+// create LINE SDK client
+const client = new Line.messagingApi.MessagingApiClient({
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN as string
+})
+
+function handleEvent(event: Line.MessageEvent) {
+  console.log('event', event)
+}
 
 export default new Elysia()
   .use(swagger())
+  .onRequest(() => {
+    Line.middleware(config)
+  })
   .get('/', () => 'Hello')
-  .post('/api/v1/webhook', ({ body }) => console.log(body))
+  .post('/api/v1/webhook', ({ body }) => console.log(body.events))
+  .post('/api/v1/callback', async ({ body }) => {
+    await Promise.all(body)
+  })
   .compile()
